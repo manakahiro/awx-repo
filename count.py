751b20d03687a328124d8b1f42db7ca5b72d518d
Hiro Manaka <manakahiro@infoseek.jp>
2025-04-12 19:49:45 +0900

    commit

--- count.py
+++ count.py
@@ -1,7 +1,14 @@
 #
 #
 import os
+import configparser
 import requests
 import datetime
 import dateutil.parser
+conf = configparser.ConfigParser()
+conf.read("Token.ini")
+base = conf["DEFAULT"]["BaseURL"]
+token = conf["DEFAULT"]["Token"]
+
+print( base )
 print( sum( [x for x in range(1, 100 + 1)] ))
