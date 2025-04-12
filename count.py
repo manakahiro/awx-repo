631884f76a27b3936b79ed78d88654287da90f8c
Hiro Manaka <manakahiro@infoseek.jp>
2025-04-12 19:59:33 +0900

    commit

--- count.py
+++ count.py
@@ -12,3 +12,10 @@ token = conf["DEFAULT"]["Token"]
 
 print( base )
 print( sum( [x for x in range(1, 100 + 1)] ))
+
+headers = { 'Authorization' : f'Bearer {token}' }
+
+r = requests.get(base + "/api/v2/jobs/?order_by=-created&page_size=10", headers = headers)
+
+if r.ok:
+    print( r.text )
